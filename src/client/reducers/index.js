bc3699211b432e189984ce843e893ae0fad61e7d
esikkala <esko.ikkala@aalto.fi>
2020-06-10 15:32:17 +0300

    Fix reducer paths

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -7,13 +7,13 @@ import animation from './animation'
 import leafletMap from './leafletMap'
 // portal spefic reducers:
 import fullTextSearch from './emlo/fullTextSearch'
-import clientSideFacetedSearch from './sampo/clientSideFacetedSearch'
+import clientSideFacetedSearch from './emlo/clientSideFacetedSearch'
 import perspective1 from './emlo/perspective1' // copy of manuscripts
 import perspective2 from './emlo/perspective2' // copy of works
 import perspective3 from './emlo/perspective3' // copy of events
 import places from './emlo/places'
 import perspective1Facets from './emlo/perspective1Facets'
-import perspective1FacetsConstrainSelf from './sampo/perspective1FacetsConstrainSelf'
+import perspective1FacetsConstrainSelf from './emlo/perspective1FacetsConstrainSelf'
 import perspective2Facets from './emlo/perspective2Facets'
 import perspective3Facets from './emlo/perspective3Facets'
 
