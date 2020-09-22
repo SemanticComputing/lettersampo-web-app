3724f8236d147852c23869f94fbc12c32de79cb1
Petri Leskinen <petri.leskinen@aalto.fi>
2020-09-22 15:20:13 +0300

    Fixed full text search

--- src/server/sparql/emlo/sparql_queries/SparqlQueriesFullText.js
+++ src/server/sparql/emlo/sparql_queries/SparqlQueriesFullText.js
@@ -1,38 +1,13 @@
 export const fullTextSearchProperties = `
-{
-    ?id a ?type__id .
-    ?type__id rdfs:label|skos:prefLabel ?type__prefLabel_ .
-    BIND(STR(?type__prefLabel_) AS ?type__prefLabel)  # ignore language tags
-  }
-  UNION
-  {
-    ?id dct:source ?source__id .
-    ?source__id skos:prefLabel ?source__prefLabel .
-    BIND(?source__id AS ?source__dataProviderUrl)
-  }
-  UNION
-  {
-    {
-      ?id a crm:E21_Person 
-    } UNION {
-      ?d a crm:E74_Group 
-    }
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/actors/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  UNION
-  {
-    ?id a crm:E53_Place .
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/places/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  UNION
-  {
-    ?id a eschema:Letter .
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/letters/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  `
+VALUES (?type__id ?type__prefLabel ?pagetype) 
+{ 
+  (crm:E21_Person "Person" "/actors")
+  (crm:E74_Group "Group" "/actors")
+  (crm:E53_Place "Place" "/places")
+}
+
+?id a ?type__id .  
+?id skos:prefLabel ?prefLabel__id .
+BIND(?prefLabel__id as ?prefLabel__prefLabel)
+BIND(CONCAT(?pagetype, "/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+`
