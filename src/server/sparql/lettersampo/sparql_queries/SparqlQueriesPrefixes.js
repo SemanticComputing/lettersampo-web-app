677d00730a0304d689a33ea64fe07c87fd1d0022
Petri Leskinen <petri.leskinen@aalto.fi>
2022-02-16 16:31:07 +0200

    Update queries

--- src/server/sparql/lettersampo/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/lettersampo/sparql_queries/SparqlQueriesPrefixes.js
@@ -1,9 +1,9 @@
 export const prefixes = `
 PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
 PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX emlo_id: <http://emlo.bodleian.ox.ac.uk/id/> 
 PREFIX eschema: <http://emlo.bodleian.ox.ac.uk/schema#> 
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX emlo_id: <http://emlo.bodleian.ox.ac.uk/id/>
 PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#> 
 PREFIX places: <http://ldf.fi/ckcc/places/>
@@ -11,13 +11,13 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX sch: <http://schema.org/>
 PREFIX sdmx-code: <http://purl.org/linked-data/sdmx/2009/code#>
-PREFIX spatial: <http://jena.apache.org/spatial#>
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX spatial: <http://jena.apache.org/spatial#>
 PREFIX text: <http://jena.apache.org/text#>
 PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-PREFIX lssc: <http://ldf.fi/schema/lssc/>
 PREFIX actors: <http://ldf.fi/ckcc/actors/>
 PREFIX letters: <http://ldf.fi/ckcc/letters/> 
+PREFIX lssc: <http://ldf.fi/schema/lssc/>
 `
