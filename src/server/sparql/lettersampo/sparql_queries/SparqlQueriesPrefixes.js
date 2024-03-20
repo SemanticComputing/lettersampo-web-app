7d601ebe479af9778cef62f1022f544e669b99b9
Petri Leskinen <petri.leskinen@aalto.fi>
2024-03-20 15:17:16 +0200

    queries

--- src/server/sparql/lettersampo/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/lettersampo/sparql_queries/SparqlQueriesPrefixes.js
@@ -13,7 +13,6 @@ PREFIX spatial: <http://jena.apache.org/spatial#>
 PREFIX text: <http://jena.apache.org/text#>
 PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
 PREFIX lssc: <http://ldf.fi/schema/lssc/>
 PREFIX actors: <http://ldf.fi/lssc/actors/>
 PREFIX letters: <http://ldf.fi/lssc/letters/>
